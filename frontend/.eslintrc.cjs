c5e7e6031a971e438f2389d1940a9f75f8692177
Randol-79 <119770852+Randol-79@users.noreply.github.com>
2026-01-29 03:36:13 -0600

    chore(frontend): support legacy ESLint config on Node 20 (add .eslintrc.cjs, remove flat config)

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,2 @@
+// CommonJS wrapper to let ESLint load legacy JSON config without ESM import assertions
+module.exports = require('./.eslintrc.json');
--- frontend/eslint.config.cjs
+++
@@ -1,3 +0,0 @@
-// Bridge config for ESLint to work under Node 20+ ESM loader
-// CommonJS wrapper that reuses existing .eslintrc.json
-module.exports = require('./.eslintrc.json');
